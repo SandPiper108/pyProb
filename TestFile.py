cb15c02f6205d5748ce20912b811d26cb165bef7
SandPiper108 <pfleming22@bentleyschool.org>
2021-06-18 11:13:18 -0700

    my messages

---
+++ TestFile.py
@@ -0,0 +1,21 @@
+import numpy as np
+print('Hello')
+
+a = 1
+
+print(a)
+rabbit = 5
+
+b = 2
+c = np.array([1,2,3])
+
+d = c+b
+print('hi')
+d = rabbit + 2
+e = rabbit + 5
+f  = rabbit + 6
+g  = rabbit + 7
+
+
+
+
