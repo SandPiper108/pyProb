317095156cd2c47a22803c43e27509cb76b5fbcd
SandPiper108 <pfleming22@bentleyschool.org>
2021-07-08 09:49:20 -0700

    Created functions and working on try/except

--- JSONPractice.py
+++ JSONPractice.py
@@ -1,12 +1,26 @@
 import json
 
-size_file = open("size_data.json", "r")
-json_sizes = json.load(size_file)
-size_file.close()
+def read(fileName): #Func to read in the new file, opens new dictionary if empty
+    new_file = open(fileName, "r")
+    json_object = json.load(new_file)
+    new_file.close()
+    try:
+        return json_object
+    except JSONDecodeError:
+        return {}
 
-json_sizes["particle1"] = 35
-print(json_sizes)
+def write(fileName, objName): #Func to change new file, req defining obj below
+    new_file = open(fileName, "w")
+    json.dump(objName, new_file, indent=4)
+    new_file.close()
 
-size_file = open("size_data.json", "w")
-json.dump(json_sizes, size_file)
-size_file.close()
+def main():
+    a = "size_data.json"
+    size_data = read(a)
+    size_data["particle1"] = 35
+    size_data["particle2"] = 56
+    write("size_data.json", size_data)
+    print(size_data)
+
+if __name__ == "__main__":
+    main()
